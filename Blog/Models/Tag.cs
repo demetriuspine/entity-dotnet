542516167dca4a76e8e1c472122b416f16c6de0e
lucas <mr.pine97@gmail.com>
2022-12-22 13:44:32 -0300

    data anotations tag

--- Blog/Models/Tag.cs
+++ Blog/Models/Tag.cs
@@ -7,8 +7,17 @@ namespace Blog.Models
     class Tag
     {
         [Key] // data annotaions: são dependentes
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // indica que a chave será gerada pelo banco
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "VARCHAR")]
         public string Name { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Slug", TypeName = "VARCHAR")]
         public string Slug { get; set; }
     }
 }
