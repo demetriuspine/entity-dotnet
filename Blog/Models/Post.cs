f49dc8265f31bc3815a15e76cfe0f8c93941b1c4
lucas <mr.pine97@gmail.com>
2022-12-22 10:39:18 -0300

    post - finaliza colunas

--- Blog/Models/Post.cs
+++ Blog/Models/Post.cs
@@ -11,20 +11,34 @@ namespace Blog.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [ForeignKey("CategoryId")] // por padrão, o EF vai dividir por maiúscula, sendo `Category` a classe e `Id` a propriedade, A classe Category deve estar mapeada, bem como a propriedade
-        [Required] // not null
-        [MinLength(3)] // mesmo não existindo essa condição no banco, ele dará erro se tentarmos salvar uma categoria com menos de 3 caracteres
-        [MaxLength(80)]
-        [Column("Name", TypeName = "NVARCHAR")]
         public int CategoryId { get; set; }
         public Category Category { get; set; } // fazendo isso, o EF já entende que é uma propriedade de navegação (ajuda a fazer joins)
         [ForeignKey("AuthorId")] // por padrão, o EF vai dividir por maiúscula, sendo `Author` a classe e `Id` a propriedade, A classe Author deve estar mapeada, bem como a propriedade
-        public User Author { get; set; } // fazendo isso, o EF já entende que é uma propriedade de navegação (ajuda a fazer joins), e o EF já entende que Author é User
         public int AuthorId { get; set; }
+        public User Author { get; set; } // fazendo isso, o EF já entende que é uma propriedade de navegação (ajuda a fazer joins), e o EF já entende que Author é User
+        [Required]
+        [MinLength(3)]
+        [MaxLength(170)]
+        [Column("Title", TypeName = "VARCHAR")]
         public string Title { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(255)]
+        [Column("Summary", TypeName = "VARCHAR")]
         public string Summary { get; set; }
+        [Required]
+        [Column("Body", TypeName = "TEXT")]
         public string Body { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Slug", TypeName = "VARCHAR")]
         public string Slug { get; set; }
+        [Required]
+        [Column("CreateDate", TypeName = "DATETIME")]
         public DateTime CreateDate { get; set; }
+        [Required]
+        [Column("LastUpdateDate", TypeName = "DATETIME")]
         public DateTime LastUpdateDate { get; set; }
     }
 }
