6333aa9cfacea8fea2881317dff7470b440c342d
lucas <mr.pine97@gmail.com>
2022-12-22 13:44:47 -0300

    mapeando algumas tabelas apenas

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -7,12 +7,8 @@ namespace Blog.Data
     {
         public DbSet<Category> Categories { get; set; } // como aqui está `Categories`, é preciso de dataAnnotation indicando que a coluna se chama Category
         public DbSet<Post> Posts { get; set; }
-        // public DbSet<PostTag> PostTags { get; set; } // não tem chave primária, é composta, é necessário fazer uma outra configuração pois o EF não deixa
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<Tag> Tags { get; set; }
         public DbSet<User> Users { get; set; }
-        // public DbSet<UserRole> UserRoles { get; set; } // não tem chave primária, é composta, é necessário fazer uma outra configuração pois o EF não deixa
-
+        public DbSet<Tag> Tags { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
