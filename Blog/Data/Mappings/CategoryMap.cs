fc65a4b47271b15e0ecc039eb8b22f2e47bfc1db
lucas <mr.pine97@gmail.com>
2023-01-02 17:50:01 -0300

    mapeia nome de tabela, chave primaria e autoincremento

---
+++ Blog/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,16 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd().UseIdentityColumn(); // autoincremento
+        }
+    }
+}
