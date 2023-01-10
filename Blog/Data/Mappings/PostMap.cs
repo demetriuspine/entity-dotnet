5016a6101c496bc88c0c049a3513ecf1be659cc6
lucas <mr.pine97@gmail.com>
2023-01-10 13:40:52 -0300

    relacionamento n para n

--- Blog/Data/Mappings/PostMap.cs
+++ Blog/Data/Mappings/PostMap.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -64,6 +64,22 @@ namespace Blog.Data.Mappings
                 .WithMany(x => x.Posts)
                 .HasConstraintName("FK_Post_Category")
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>( // criação tabela associativa (entitade virtual)
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                );
         }
     }
 }
