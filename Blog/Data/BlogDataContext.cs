0def18db57cf7be4e4cbc4f40f98bc3661abbc22
lucas <mr.pine97@gmail.com>
2023-01-12 14:29:45 -0300

    configurando o datacontext

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -1,4 +1,5 @@
 using System;
+using Blog.Data.Mappings;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,10 +7,20 @@ namespace Blog.Data
 {
     class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
             optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
         }
     }
 }
